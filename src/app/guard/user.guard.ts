bda17b8fe70ccc11273d69f2cf42538fba61ff0c
Ricardo Gimenez <ricardo.gimenez096@gmail.com>
2024-10-08 10:17:22 -0300

    feat: Add router guards components

---
+++ src/app/guard/user.guard.ts
@@ -0,0 +1,23 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { toast } from 'ngx-sonner';
+
+
+
+export const userGuard: CanActivateFn = (route, state) => {
+  
+  const router = inject(Router);
+
+  const user = JSON.parse(localStorage.getItem('user'));
+  
+  console.log(user.rol);
+  
+  if(user.rol !== 'admin'){
+    toast.error('No tienes permisos para ingresar')
+    router.navigate(['/dashboard']);
+    return false;
+
+  }
+
+  return true;
+};
